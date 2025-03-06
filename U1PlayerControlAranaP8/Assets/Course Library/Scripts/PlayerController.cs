ff20672a8b591798dd2112b2273b7585b36c0be4
luiss1234 <756101@student.dusd.net>
2025-03-06 15:19:58 -0800

    I finished more then half of my the game
    I'm almost done and I made my car move.

--- U1PlayerControlAranaP8/Assets/Course Library/Scripts/PlayerController.cs
+++ U1PlayerControlAranaP8/Assets/Course Library/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Verticle");
+
         // We'll move the vehicle foward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
